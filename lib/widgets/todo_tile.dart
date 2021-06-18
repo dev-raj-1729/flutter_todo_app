a68f8a1e82678eced6725d5c134c893690598fed
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-06-18 13:57:18 +0530

    added confirmDismiss

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -13,6 +13,36 @@ class TodoTile extends StatelessWidget {
     return Dismissible(
       onDismissed: (_) => Provider.of<Todos>(context, listen: false)
           .removeItemById(todoItem.id),
+      confirmDismiss: (_) => showDialog<bool?>(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('Delete todo?'),
+                content: Text('Are you sure you want to delete this todo?'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                    child: Text('Yes'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                    child: Text('No'),
+                  ),
+                ],
+              )),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        padding: EdgeInsets.only(left: 20),
+        alignment: Alignment.centerLeft,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 35,
+        ),
+      ),
       key: Key(todoItem.id),
       child: Card(
         elevation: 5,
